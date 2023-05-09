766cba3f2b3109e9491d548df46db8dc16bd473b
nico <->
2023-05-09 10:30:34 +0200

    CategoryFilter

--- src/main/java/ohm/softa/a08/filtering/CategoryFilter.java
+++ src/main/java/ohm/softa/a08/filtering/CategoryFilter.java
@@ -3,9 +3,22 @@ package ohm.softa.a08.filtering;
 import ohm.softa.a08.model.Meal;
 import org.apache.commons.lang3.NotImplementedException;
 
-public class CategoryFilter extends FilterBase{
+import java.util.List;
+
+public class CategoryFilter extends FilterBase {
+	private final boolean include;
+	private final List<String> categories;
+
+	public CategoryFilter(boolean include, List<String> categories) {
+		this.include = include;
+		this.categories = categories;
+	}
+
 	@Override
 	protected boolean include(Meal m) {
-	throw new NotImplementedException();
+		if (include) {
+			return categories.stream().anyMatch(e -> e.equalsIgnoreCase(m.getCategory()));
+		}
+		return categories.stream().noneMatch(e -> e.equalsIgnoreCase(m.getCategory()));
 	}
 }
