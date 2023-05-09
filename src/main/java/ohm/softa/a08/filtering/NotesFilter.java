0af0e3982e407c3d7df544ae25617e83b8fb804b
nico <->
2023-05-09 10:37:43 +0200

    NotesFilter

--- src/main/java/ohm/softa/a08/filtering/NotesFilter.java
+++ src/main/java/ohm/softa/a08/filtering/NotesFilter.java
@@ -1,4 +1,26 @@
 package ohm.softa.a08.filtering;
 
-public class NotesFilter {
+import ohm.softa.a08.model.Meal;
+
+import java.util.List;
+
+public class NotesFilter extends FilterBase {
+
+	private final boolean include;
+
+	private final List<String> notes;
+
+	public NotesFilter(boolean include, List<String> notes) {
+		this.include = include;
+		this.notes = notes;
+	}
+
+
+	@Override
+	protected boolean include(Meal m) {
+		if (include) {
+			return notes.stream().anyMatch(e -> m.getNotes().contains(e));
+		}
+		return notes.stream().noneMatch(e -> m.getNotes().contains(e));
+	}
 }
