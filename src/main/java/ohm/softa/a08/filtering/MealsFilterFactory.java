694d8b36daadbce206ce1ca07dabbf49e46ece37
nico <->
2023-05-09 10:53:01 +0200

    FilterFactory

--- src/main/java/ohm/softa/a08/filtering/MealsFilterFactory.java
+++ src/main/java/ohm/softa/a08/filtering/MealsFilterFactory.java
@@ -1,4 +1,18 @@
 package ohm.softa.a08.filtering;
 
+import java.util.*;
+
 public class MealsFilterFactory {
+	private static final Map<String, MealsFilter> filters = new HashMap<String, MealsFilter>();
+
+	static {
+		filters.put("all", new NoFilter());
+		filters.put("no pork", new CategoryFilter(false, List.of("Schwein")));
+		filters.put("no soy", new NotesFilter(false, List.of("mit Soja")));
+		filters.put("vegetarian", new CategoryFilter(true, List.of("vegetarisch", "vegan")));
+	}
+
+	public static MealsFilter getStrategy(String key) {
+		return filters.getOrDefault(key, new NoFilter());
+	}
 }
